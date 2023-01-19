9029a5b330fe3ecbaf445b528acaaa7c75bb4d53
S Ranjit Rao <ranjitrao2702@gmail.com>
2023-01-19 22:04:22 +0530

    Using useMemo

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Header = () => {
+    return <h1 className='f1'>RoboFriends</h1>
+}
+
+export default React.memo(Header)
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+import Header from '../components/Header';
 import './App.css';
 
 import { requestRobots, setSearchField } from '../actions';
@@ -36,7 +37,7 @@ const App = ({ searchField, onSearchChange, robots, isPending, onRequestRobots }
         <h1>Loading</h1> :
         (
             <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
+                <Header />
                 <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundary>
